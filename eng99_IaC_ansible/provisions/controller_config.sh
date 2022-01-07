b329c4e02cb4037d811308eafed0f1ef9344ef9b
schowdhury2019 <sunnychowdhury@hotmail.com>
2022-01-07 17:32:05 +0000

    Commit

--- eng99_IaC_ansible/provisions/controller_config.sh
+++ eng99_IaC_ansible/provisions/controller_config.sh
@@ -1,6 +1,11 @@
+#!/bin/bash
+
 # Update and upgrade
 sudo apt-get update -y
 sudo apt-get upgrade -y
+
+# Install tree
+sudo apt-get install tree -y
 
 # Install remaining stuff
 sudo apt-get install software-properties-common -y
@@ -8,6 +13,14 @@ sudo apt-add-repository ppa:ansible/ansible
 
 # Install ansible
 sudo apt-get install ansible -y
+
+# Install python for ansible vault
+sudo apt install python3-pip -y
+
+# Install aws cli
+pip3 install awscli
+pip3 install boto boto3
+sudo apt-get upgrade -y
 
 # Replace hosts file
 sudo mv /etc/ansible/hosts /etc/ansible/original_hosts
--- eng99_IaC_ansible/provisions/web_config.sh
+++ eng99_IaC_ansible/provisions/web_config.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Update and upgrade
 sudo apt-get update -y
 sudo apt-get upgrade -y
